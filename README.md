2c4c413f0368eaef1fc0debc22d36a5653e76c89
Aiden Mackean <ajmackean@gmail.com>
2021-06-28 11:42:14 -0400

    gson added

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 -----------
-MUSKO COIN |
+MUSKO COIN 
 -----------
 
 Musko Coin is a test implementation of a proof-of-work blockchain protocol for eductional purposes. 
+
+Integrated gson library for object manipulation
--- src/Muskchain.java
+++ src/Muskchain.java
@@ -1,16 +1,20 @@
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
 
 public class Muskchain {
+	
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
 	public static void main(String[] args) {
 		
-		Block genesisBlock = new Block("initial block", "0");
-		System.out.println("Hash for block 1: " + genesisBlock.hash);
+		blockchain.add(new Block("genesis block", "0"));		
+		blockchain.add(new Block("second block",blockchain.get(blockchain.size()-1).hash)); 
+		blockchain.add(new Block("third block",blockchain.get(blockchain.size()-1).hash));
 		
-		Block secondBlock = new Block("second block", genesisBlock.hash);
-		System.out.println("Hash for block 2: " + secondBlock.hash);
-		
-		Block thirdBlock = new Block("third block", secondBlock.hash);
-		System.out.println("Hash for block 3: " + thirdBlock.hash);
+		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
+		System.out.println(blockchainJson);
+
 
 	}
 
