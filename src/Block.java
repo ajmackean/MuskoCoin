446859a2bfbb4ca813d96f644cc8d5626f7379ee
Aiden Mackean <ajmackean@gmail.com>
2021-06-28 10:34:35 -0400

    first commit

---
+++ src/Block.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Block {
+	
+	public String hash;
+	public String previousHash;
+	private String data; // this is just a simple message for now. 
+	private long timeStamp; // uses the date from util package
+	
+	// constructor. Implementation of a linked list data structure 
+	public Block ()
+
+}
